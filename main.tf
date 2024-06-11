dd10209061d7239bc79b1b62d7f7f1905ffc0d6f
Greg <gregeast94@gmail.com>
2024-06-11 15:28:35 -0400

    Uploading main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#create EC2 resource
+
+resource "aws_instance" "myWebsite" {
+  ami           = "ami-08a0d1e16fc3f61ea"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
