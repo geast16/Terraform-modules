3372a2267a89ebdb45c59ad790c32295d7ff4854
Greg <gregeast94@gmail.com>
2024-06-13 13:58:49 -0400

    terraform import

--- task3/main.tf
+++ task3/main.tf
@@ -287,3 +287,54 @@ resource "aws_security_group" "vpc-ping" {
   }
 }
 
+resource "random_string" "random1" {
+  length      = 12
+  special     = true
+  min_numeric = 6
+  min_special = 2
+  min_upper   = 3
+}
+
+# Terraform Resource Block - To Build Web Server in Public Subnet
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
+  aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.generated.key_name
+  connection {
+    user        = "ubuntu"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
+
+  # Leave the first part of the block unchanged and create our `local-exec` provisioner
+  provisioner "local-exec" {
+    command = "chmod 600 ${local_file.private_key_pem.filename}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rm -rf /tmp",
+      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
+      "sudo sh /tmp/assets/setup-web.sh",
+    ]
+  }
+
+  tags = {
+    Name = "Web EC2 Server"
+  }
+
+  lifecycle {
+    ignore_changes = [security_groups]
+  }
+
+}
+
+resource "aws_instance" "aws_linux" {
+  instance_type = "t2.micro"
+  ami           = "ami-08a0d1e16fc3f61ea"
+
+}
